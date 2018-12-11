8912ebab3bd56f57a70e54e5ed93a8a508ef42bf
seendsouza <sdammobubbles@gmail.com>
2018-12-10 22:42:34 -0500

    moved drives to functions and added split arcade drive

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -19,15 +19,50 @@
 #define LEFT_BACK_WHEELS_PORT 3
 #define RIGHT_BACK_WHEELS_PORT 4
 
-void opcontrol() {
-	while (true) {
-        int left = controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y);
-        int right = controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_Y);
-        right *= -1 // Reversing the right motor
-        motor_move(LEFT_FRONT_WHEELS_PORT, left)
-        motor_move(LEFT_BACK_WHEELS_PORT, left)
-        motor_move(RIGHT_FRONT_WHEELS_PORT, right)
-        motor_move(RIGHT_BACK_WHEELS_PORT, right)
-		delay(2);
-	}
+/* Drive Types:
+ * 0 is Tank Drive
+ * 1 is Split Arcade Drive
+ * 2 is Arcade Drive
+ */
+
+int drive_type = 0;
+
+void opcontrol () {
+    while (true) {
+        if (drive_type == 0){
+            tank_drive()
+        } else if (drive_type == 1) {
+            split_arcade_drive()
+        }
+    }
+}
+
+void tank_drive() {
+    int left = controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y);
+    int right = controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_Y);
+    
+    right *= -1 // Reversing the right motor
+    
+    motor_move(LEFT_FRONT_WHEELS_PORT, left);
+    motor_move(LEFT_BACK_WHEELS_PORT, left);
+    motor_move(RIGHT_FRONT_WHEELS_PORT, right);
+    motor_move(RIGHT_BACK_WHEELS_PORT, right);
+	
+    delay(2);
+}
+
+void split_arcade_drive() {
+    int power = controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y);
+    int turn = controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_X);
+    int left = power + turn;
+    int right = power - turn;
+    
+    right *= -1 // Reversing the right motor
+    
+    motor_move(LEFT_FRONT_WHEELS_PORT, left);
+    motor_move(LEFT_BACK_WHEELS_PORT, left);
+    motor_move(RIGHT_FRONT_WHEELS_PORT, right);
+    motor_move(RIGHT_BACK_WHEELS_PORT, right);
+   
+    delay(2)
 }
