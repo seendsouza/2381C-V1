57b71d7430d26d509fa491f967b4aec4fb32747f
seendsouza <sdammobubbles@gmail.com>
2018-12-13 01:22:38 -0500

    attempted to fix scopes (unsuccessful)

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -13,15 +13,15 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+pros::Motor left_front_wheels (LEFT_FRONT_WHEELS_PORT);
+pros::Motor right_front_wheels (RIGHT_FRONT_WHEELS_PORT);
+pros::Motor left_back_wheels (LEFT_BACK_WHEELS_PORT);
+pros::Motor right_back_wheels (RIGHT_BACK_WHEELS_PORT);
+pros::Motor lift_left_motor (LEFT_LIFT_PORT); // The arm motor has the 200 rpm gearset
+pros::Motor lift_right_motor (RIGHT_LIFT_PORT); // The arm motor has the 200 rpm gearset
+pros::Motor claw_motor (CLAW_PORT); // The arm motor has the 200 rpm gearset
+pros::Controller master (CONTROLLER_MASTER);
 void opcontrol () {
-    pros::Motor left_front_wheels (LEFT_FRONT_WHEELS_PORT);
-    pros::Motor right_front_wheels (RIGHT_FRONT_WHEELS_PORT);
-    pros::Motor left_back_wheels (LEFT_BACK_WHEELS_PORT);
-    pros::Motor right_back_wheels (RIGHT_BACK_WHEELS_PORT);
-    pros::Motor lift_left_motor (LEFT_LIFT_PORT); // The arm motor has the 200 rpm gearset
-    pros::Motor lift_right_motor (RIGHT_LIFT_PORT); // The arm motor has the 200 rpm gearset
-    pros::Motor claw_motor (CLAW_PORT); // The arm motor has the 200 rpm gearset
-    pros::Controller master (CONTROLLER_MASTER);
     
     while (true) {
         left_back_wheels.move(master.get_analog(ANALOG_LEFT_Y));
@@ -50,7 +50,7 @@ void opcontrol () {
         pros::delay(20);
     }
 }
-/*
+
 void split_arcade_drive() {
     int power = master.get_analog(ANALOG_LEFT_Y);
     int turn = master.get_analog(ANALOG_RIGHT_X);
@@ -60,8 +60,6 @@ void split_arcade_drive() {
     right_back_wheels.move(master.get_analog(right));
     left_front_wheels.move(master.get_analog(left));
     right_front_wheels.move(master.get_analog(right));
-
-    pros::delay(20);
 }
 
 void arcade_drive() {
@@ -73,7 +71,4 @@ void arcade_drive() {
     right_back_wheels.move(master.get_analog(right));
     left_front_wheels.move(master.get_analog(left));
     right_front_wheels.move(master.get_analog(right));
-
-    pros::delay(20);
 }
-*/
