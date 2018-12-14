b4270dd2d471232c532daea10f722dba3eb3c01c
Victor Wang <45219858+vdoubleu@users.noreply.github.com>
2018-12-13 22:20:29 -0500

    Update autonomous.cpp

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -11,6 +11,7 @@
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+
 void autonomous() {
     pros::Motor left_front_wheels (LEFT_FRONT_WHEELS_PORT);
     pros::Motor right_front_wheels (RIGHT_FRONT_WHEELS_PORT);
@@ -18,20 +19,44 @@ void autonomous() {
     pros::Motor right_back_wheels (RIGHT_BACK_WHEELS_PORT);
     pros::Controller master (CONTROLLER_MASTER);
 
-    int drive_forward = 1000;
-    int drive_left = 1000;
-    int drive_forward_2 = 1000;
+    int drive_forward = 100; //drive distance
+    int drive_left = 100;
+    int drive_forward_2 = 100;
 
-    left_back_wheels.move(drive_forward);
-    right_back_wheels.move(drive_forward);
-    left_front_wheels.move(drive_forward);
-    right_front_wheels.move(drive_forward);
+    left_back_wheels.move_relative(drive_forward, 100); //drive forward 100 tick at 100 rpm
+    right_back_wheels.move_relative(drive_forward, 100);
+    left_front_wheels.move_relative(drive_forward, 100);
+    right_front_wheels.move_relative(drive_forward, 100);
 
-    right_back_wheels.move(drive_left);
-    right_front_wheels.move(drive_left);
+    while (right_front_wheels.get_position()-drive_forward > 5 && left_front_wheels.get_position()-drive_forward > 5 ) {
+        pros::delay(2);
+    }
 
-    left_back_wheels.move(drive_forward_2);
-    right_back_wheels.move(drive_forward_2);
-    left_front_wheels.move(drive_forward_2);
-    right_front_wheels.move(drive_forward_2);
+    left_back_wheels.tare_position(); // set motor position to zero
+    right_back_wheels.tare_position();
+    left_front_wheels.tare_position();
+    right_front_wheels.tare_position();
+
+    right_front_wheels.move_relative(drive_left, 100); //turn left
+    right_back_wheels.move_relative(drive_left, 100);
+    left_back_wheels.move_relative(-drive_left, 100);
+    left_front_wheels.move_relative(-drive_left, 100);
+
+    while (right_front_wheels.get_position()-drive_left > 5 && left_front_wheels.get_position() + drive_left > 5 ) {
+        pros::delay(2);
+    }
+
+    left_back_wheels.tare_position();
+    right_back_wheels.tare_position();
+    left_front_wheels.tare_position();
+    right_front_wheels.tare_position();
+
+    left_back_wheels.move_relative(drive_forward, 100); //drive forward 100 tick at 100 rpm
+    right_back_wheels.move_relative(drive_forward, 100);
+    left_front_wheels.move_relative(drive_forward, 100);
+    right_front_wheels.move_relative(drive_forward, 100);
+
+    while (right_front_wheels.get_position()-drive_forward_2 > 5 && left_front_wheels.get_position()-drive_forward_2 > 5 ) {
+        pros::delay(2);
+    }
 }
