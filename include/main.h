c62f728a6d5bab28089c33e5937571c41bc23802
Sean D'Souza <38793207+seendsouza@users.noreply.github.com>
2018-12-14 00:08:26 -0500

    Revert "Drive into functions"

--- include/main.h
+++ include/main.h
@@ -74,8 +74,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
 void autonomous(void);
 void initialize(void);
 void disabled(void);
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,7 +1,4 @@
 #include "main.h"
-#ifndef "motorinitialize.cpp"
-#include "motorinitialize.cpp"
-#endif
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -16,41 +13,22 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-
-
-void tank_drive() {
-    left_back_wheels.move(master.get_analog(ANALOG_LEFT_Y));
-    right_back_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
-    left_front_wheels.move(master.get_analog(ANALOG_LEFT_Y));
-    right_front_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
-}
-
-void split_arcade_drive() {
-    int power = master.get_analog(ANALOG_LEFT_Y);
-    int turn = master.get_analog(ANALOG_RIGHT_X);
-    int left = power + turn;
-    int right = power - turn;
-    left_back_wheels.move(left);
-    right_back_wheels.move(right);
-    left_front_wheels.move(left);
-    right_front_wheels.move(right);
-}
-
-void arcade_drive() {
-    int power = master.get_analog(ANALOG_LEFT_Y);
-    int turn = master.get_analog(ANALOG_LEFT_X);
-    int left = power + turn;
-    int right = power - turn;
-    left_back_wheels.move(left);
-    right_back_wheels.move(right);
-    left_front_wheels.move(left);
-    right_front_wheels.move(right);
-}
-
 void opcontrol () {
-    while (true) {       
-        tank_drive();
-
+    pros::Motor left_front_wheels (LEFT_FRONT_WHEELS_PORT);
+    pros::Motor right_front_wheels (RIGHT_FRONT_WHEELS_PORT);
+    pros::Motor left_back_wheels (LEFT_BACK_WHEELS_PORT);
+    pros::Motor right_back_wheels (RIGHT_BACK_WHEELS_PORT);
+    pros::Motor lift_left_motor (LEFT_LIFT_PORT); // The arm motor has the 200 rpm gearset
+    pros::Motor lift_right_motor (RIGHT_LIFT_PORT); // The arm motor has the 200 rpm gearset
+    pros::Motor claw_motor (CLAW_PORT); // The arm motor has the 200 rpm gearset
+    pros::Controller master (CONTROLLER_MASTER);
+    
+    while (true) {
+        left_back_wheels.move(master.get_analog(ANALOG_LEFT_Y));
+        right_back_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
+        left_front_wheels.move(master.get_analog(ANALOG_LEFT_Y));
+        right_front_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
+       
         if (master.get_digital(DIGITAL_R1)) {
             lift_left_motor.move_velocity(100);
             lift_right_motor.move_velocity(100);
@@ -69,7 +47,33 @@ void opcontrol () {
         } else {
             claw_motor.move_velocity(0);
         }  
-        
         pros::delay(20);
     }
 }
+/*
+void split_arcade_drive() {
+    int power = master.get_analog(ANALOG_LEFT_Y);
+    int turn = master.get_analog(ANALOG_RIGHT_X);
+    int left = power + turn;
+    int right = power - turn;
+    left_back_wheels.move(master.get_analog(left));
+    right_back_wheels.move(master.get_analog(right));
+    left_front_wheels.move(master.get_analog(left));
+    right_front_wheels.move(master.get_analog(right));
+
+    pros::delay(20);
+}
+
+void arcade_drive() {
+    int power = master.get_analog(ANALOG_LEFT_Y);
+    int turn = master.get_analog(ANALOG_LEFT_X);
+    int left = power + turn;
+    int right = power - turn;
+    left_back_wheels.move(master.get_analog(left));                                                                         
+    right_back_wheels.move(master.get_analog(right));
+    left_front_wheels.move(master.get_analog(left));
+    right_front_wheels.move(master.get_analog(right));
+
+    pros::delay(20);
+}
+*/
